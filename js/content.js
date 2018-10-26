0620819aa3bb76d133af088baf5e33bb0b98ae63
shaoding <shaodingzhang@gmail.com>
2018-10-26 20:09:38 +0800

    add decode

--- js/content.js
+++ js/content.js
@@ -34,9 +34,22 @@ var funDownload = function (content, filename) {
     document.body.removeChild(eleLink);
 };
 
+var htmlDecodeByRegExp = function (str){  
+          var s = "";
+          if(str.length == 0) return "";
+          s = str.replace(/&amp;/g,"&");
+          s = s.replace(/&lt;/g,"<");
+          s = s.replace(/&gt;/g,">");
+          s = s.replace(/&nbsp;/g," ");
+          s = s.replace(/&#39;/g,"\'");
+          s = s.replace(/&quot;/g,"\"");
+          return s;  
+    }
+
 var parseMath = function (content) {
+    var decode = htmlDecodeByRegExp(content);
     var reg = /<math([\S\s]*?)<\/math>/g;
-    var match = content.match(reg);
+    var match = decode.match(reg);
     var res = '';
     if (match) {
         res =  match.join('\n');
